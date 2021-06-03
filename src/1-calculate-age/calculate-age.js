0f46ab3ebf02faab088add77cc4a62eb85951afc
David Aceves <daceves.dev@gmail.com>
2021-06-03 00:29:42 -0400

    calculate-age

--- src/1-calculate-age/calculate-age.js
+++ src/1-calculate-age/calculate-age.js
@@ -1,4 +1,29 @@
 // Write a function that returns the current age of the person in years.
 exports.ageAtDeath = function (birthDate, deathDate) {
+  const birthYear = birthDate.getUTCFullYear();
+  const deathYear = deathDate.getUTCFullYear();
+
+  const birthMonth = birthDate.getMonth();
+  const deathMonth = deathDate.getMonth();
+
+  const birthDayOfWeek = birthDate.getDate();
+  const deathDayOfWeek = deathDate.getDate();
+
+  let age = deathYear - birthYear;
+
+  if (birthMonth < deathMonth) {
+    // If death month is past birthday then you can return age
+    return age;
+  } else if (birthMonth > deathMonth) {
+    // If birthday month has not come then subtract age by 1
+    return age - 1;
+  } else if (birthMonth === deathMonth && birthDayOfWeek > deathDayOfWeek) {
+    // If the same month and birthday has not yet come subtract age by 1
+    return age - 1;
+  } else if (birthMonth === deathMonth && birthDayOfWeek <= deathDayOfWeek) {
+    // If same month and birthday is same as death day or greater return age
+    return age;
+  }
+
   return 0;
-}
+};
--- src/1-calculate-age/calculate-age.test.js
+++ src/1-calculate-age/calculate-age.test.js
@@ -1,13 +1,17 @@
-const { ageAtDeath } = require('./calculate-age');
+const { ageAtDeath } = require("./calculate-age");
 
-test('age 01/21/1994 to 06/01/2021 to equal 27', () => {
-  expect(
-    ageAtDeath(new Date(1994, 1, 21), new Date(2021, 6, 1))
-  ).toBe(27);
+test("age 01/21/1994 to 06/01/2021 to equal 27", () => {
+  expect(ageAtDeath(new Date(1994, 1, 21), new Date(2021, 6, 1))).toBe(27);
 });
 
-test('age 08/20/1989 to 06/01/2021 to equal 31', () => {
-  expect(
-    ageAtDeath(new Date(1989, 8, 20), new Date(2021, 6, 1))
-  ).toBe(31);
+test("age 08/20/1989 to 06/01/2021 to equal 31", () => {
+  expect(ageAtDeath(new Date(1989, 8, 20), new Date(2021, 6, 1))).toBe(31);
+});
+
+test("age 08/20/1989 to 08/21/2021 to equal 32", () => {
+  expect(ageAtDeath(new Date(1989, 8, 20), new Date(2021, 8, 21))).toBe(32);
+});
+
+test("age 08/21/1989 to 08/20/2021 to equal 31", () => {
+  expect(ageAtDeath(new Date(1989, 8, 21), new Date(2021, 8, 20))).toBe(31);
 });
