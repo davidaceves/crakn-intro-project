c381edd9e44ce1b9d648015b252ddfeab913bc00
David Aceves <daceves.dev@gmail.com>
2021-06-04 01:02:14 -0400

    submit-form

--- src/2-submit-form/index.js
+++ src/2-submit-form/index.js
@@ -1,19 +1,54 @@
-const formElement = document.getElementById('login-form')
+const formElement = document.getElementById("login-form");
 
-formElement.addEventListener("submit", function(event) {
+const inputEmail = document.getElementById("email-address");
+const errorLabelEmail = document.getElementById("error-email");
+
+const inputPassword = document.getElementById("password");
+const errorLabelPass = document.getElementById("error-pass");
+
+const errorLabels = document.getElementsByClassName("errors");
+const formControl = document.getElementsByClassName("form-control");
+
+// inputEmail.addEventListener("input", function () {
+//   if (inputEmail.value) {
+//     errorLabelEmail.innerHTML = "";
+//   }
+// });
+
+// inputPassword.addEventListener("input", function () {
+//   if (inputPassword.value) {
+//     errorLabelPass.innerHTML = "";
+//   }
+// });
+
+// Refactored
+for (let i = 0; i < formControl.length; i++) {
+  formControl[i].addEventListener("input", function () {
+    errorLabels[i].innerHTML = "";
+  });
+}
+
+formElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
   if (event.submitter) {
     event.submitter.disabled = true;
   }
 
-  console.log(event)
+  console.log(event);
 
   // validate form here
+  if (!inputEmail.value) {
+    errorLabelEmail.innerHTML = "Please enter an email address.";
+  }
+
+  if (!inputPassword.value) {
+    errorLabelPass.innerHTML = "Please enter a password.";
+  }
 
   if (event.submitter) {
     event.submitter.disabled = false;
   }
 
-  false // we never actually want to submit the form since this is a proof of concept
+  false; // we never actually want to submit the form since this is a proof of concept
 });
